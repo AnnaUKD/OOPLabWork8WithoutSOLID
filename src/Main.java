2d665ecd668ccd3d28530e0297e9e5e8674a20fa
PogoAnnaUKDIPZ <anna.i.pohoreltseva@ukd.edu.ua>
2026-04-15 10:35:24 +0300

    Add output message for CafeOrder display

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("=== БЕЗ SOLID ===");
+
         CafeOrder order = new CafeOrder("volume");
         order.addItem("Кава", 2);
         order.addItem("Круасан", 3);
